2357ef1c79d5c51114d6ff2873e995a00cb8e38d
chxragsaxena <2211301212@stu.manit.ac.in>
2025-07-23 14:56:55 +0530

    Update page.tsx

--- chatbox/src/app/chat/[id]/page.tsx
+++ chatbox/src/app/chat/[id]/page.tsx
@@ -10,7 +10,7 @@ type Props={
   params:{id:string};
 };
 export default async function chat({params}:Props) {
-  const {id} = await params;
+  const {id} = params;
   if (id.length !== 36){
     return notFound();
   }
